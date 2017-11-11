7c9f6d7dc2656585abdf2f282a395bb295e4d8a9
LisaDesoubeaux <l.desoubeaux@outlook.fr>
2017-11-12 00:47:26 +0100

    Modification classe Player

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,15 @@ public class Main {
 	public static Player p2;
 
 	public static void main(String[] args) {
-		p1 = new Player();
-		p1.nom = "Lisa";
+		
+		test();
+;
+	}
+	
+	public static void test() {
+		p1 = new Player("Lisa");
+		System.out.println(p1.asString());
+		p2 = new Player();
 		try {
 			p1.save();
 		} catch (IOException e) {
@@ -17,27 +24,15 @@ public class Main {
 		}
 		System.out.println("sauvegarde ok ");
 		try {
-			p2 = load_backup(p1.nom);
+			p2.load_backup(p1.nom);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 		System.out.print(p2.nom);
-		
-;
 	}
 	
-	// loads a player from a backup file
-	public static Player load_backup (String f) throws IOException, ClassNotFoundException{
-		Player j;
-		FileInputStream r = new FileInputStream("./SAVE/"+f); 
-		ObjectInputStream o = new ObjectInputStream(r); 
-		Object lu = o.readObject(); 
-		j = (Player)lu;
-		o.close(); 
-		r.close(); 
-		return j;
-	}
+	// select players
 
 }
--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,7 @@
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
@@ -8,7 +10,7 @@ public class Player implements Serializable  {
 
 // Attributes
 	public String nom;
-	public int nb_soldats = 1;
+	public int nb_soldats = 0;
 	public int nb_or = 1;
 	public int nb_argent = 1;
 	public int nb_petrole = 1;
@@ -44,9 +46,18 @@ public class Player implements Serializable  {
 	public void setNb_petrole(int nb_petrole) {
 		this.nb_petrole = nb_petrole;
 	}
-	
+
+// Constructors
+	public Player() {
+	}
+
+	public Player(String nom) {
+		this.nom = nom;
+	}
+
 // Other Methods
 	
+
 	// Save
 	public void save () throws IOException{
 		String f = "./SAVE/"+this.nom;
@@ -55,5 +66,26 @@ public class Player implements Serializable  {
 		o.writeObject(this);
 		o.close();
 		w.close();
+	}
+	//Load
+	public  void load_backup (String f) throws IOException, ClassNotFoundException{
+		Player p = new Player();
+		FileInputStream r = new FileInputStream("./SAVE/"+f); 
+		ObjectInputStream o = new ObjectInputStream(r); 
+		Object lu = o.readObject(); 
+		p = (Player)lu;
+		this.nom = p.nom;
+		this.nb_soldats = p.nb_soldats;
+		this.nb_or = p.nb_or;
+		this.nb_argent = p.nb_argent;
+		this.nb_petrole = p.nb_petrole;
+		o.close(); 
+		r.close(); 
+	}
+	// Print
+	public String asString () {
+		String s = " nom : "+this.nom+"\n soldats :"+this.nb_soldats+"\n or :"
+				+this.nb_or+"\n argent :"+this.nb_argent+"\n petrole :"+this.nb_petrole+"\n";
+		return s;
 	}
 }
